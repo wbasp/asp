2738eb5822b112ebb65e9260def54871852da6ee
wbasp <gokul19752006@gmail.com>
2025-03-11 12:51:20 +0530

    Update WebForm1.aspx.vb

--- Mouse Over Event/Mouse Over Event/WebForm1.aspx.vb
+++ Mouse Over Event/Mouse Over Event/WebForm1.aspx.vb
@@ -2,6 +2,12 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+myanchor.Attributes.Add("onmouseover",
+
+"child=window.open('welcome.aspx','a',top=200,left=250,width=500,height=300);")
+
+myanchor.Attributes.Add("onmouseout", "child.close();")
+
 
     End Sub
 
